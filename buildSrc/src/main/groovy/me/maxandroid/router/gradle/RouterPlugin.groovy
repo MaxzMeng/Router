c26f98b5deddfdb57dd70c2bec0964a8d33328a4
Xiangzhao Meng <maxz@XiangzhaodeMac-mini.local>
2021-01-04 22:10:17 +0800

    [NewFeature] Add extension demo
    Signed-off-by: Xiangzhao Meng <maxz@XiangzhaodeMac-mini.local>

--- buildSrc/src/main/groovy/me/maxandroid/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/me/maxandroid/router/gradle/RouterPlugin.groovy
@@ -3,12 +3,21 @@ package me.maxandroid.router.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class RouterPlugin implements Plugin<Project>{
+class RouterPlugin implements Plugin<Project> {
+
+    private static final String ROUTER_NAME = "router"
 
     @Override
     void apply(Project project) {
 
-        println("Router Plugin applied ${project.name}")
+        project.getExtensions().create(ROUTER_NAME, RouterExtension)
+
+        project.afterEvaluate {
+
+            RouterExtension extension = project[ROUTER_NAME]
+            println(extension.wikiDir)
+
+        }
 
     }
 }
